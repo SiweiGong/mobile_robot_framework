81c53eb13ba8d562672a9810306239a1519d4a71
Siwei Gong <gongsiwei1993@gmail.com>
2020-01-03 23:38:59 +0800

    added particle filter and read_data

--- odometry_based_motion_model.py
+++ odometry_based_motion_model.py
@@ -1,3 +1,6 @@
+# The odometry based motion model helps to estimate the pose of the robot 
+# after a certain movement which is represented by odometry readings
+
 import sample_function as sf
 import numpy as np
 import scipy.stats
@@ -6,7 +9,8 @@ import matplotlib.pyplot as plt
 def sample_function(mean,std):
     return sf.sample_normal_distribution_numpy(mean,std)
 
-def odometry_based_motion_model(x,u,alpha):
+def odometry_based_motion_estimate(x,u,alpha):
+    # u: (delta_rot1, delta_rot2, delta_trans) Odometry readings 
     measured_delta_rot1 = u[0] + sample_function(0,alpha[0]*np.abs(u[0])+alpha[1]*np.abs(u[-1]))
     measured_delta_rot2 = u[1] + sample_function(0,alpha[0]*np.abs(u[1])+alpha[1]*np.abs(u[-1]))
     measured_delta_trans = u[-1] + sample_function(0,alpha[2]*np.abs(u[-1])+alpha[3]*(np.abs(u[0])+np.abs(u[1])))
@@ -15,15 +19,19 @@ def odometry_based_motion_model(x,u,alpha):
     y_post = x[1] + measured_delta_trans*np.sin(x[-1]+measured_delta_rot1)
     return x_post, y_post, theta_post
 
-pose_start = np.array([2.0,4.0,0.0])
-odometry_reading_start = np.array([np.pi/2,0.0,1.0])
-noise_param = np.array([0.1,0.1,0.01,0.01])
-num_eval = 5000
-new_pose = np.empty((num_eval,3))
-plt.figure()
-for i in range(num_eval):
-    new_pose[i,:] = odometry_based_motion_model(pose_start,odometry_reading_start,noise_param)
+def main():
+    pose_start = np.array([2.0,4.0,0.0])
+    odometry_reading_start = np.array([np.pi/2,0.0,1.0])
+    noise_param = np.array([0.1,0.1,0.01,0.01])
+    num_eval = 5000
+    new_pose = np.empty((num_eval,3))
+    plt.figure()
+    for i in range(num_eval):
+        new_pose[i,:] = odometry_based_motion_estimate(pose_start,odometry_reading_start,noise_param)
 
-plt.scatter(new_pose[:,0],new_pose[:,1],s=0.1)
-plt.quiver(pose_start[0],pose_start[1],np.cos(pose_start[-1]),np.sin(pose_start[-1]))
-plt.show()
+    plt.scatter(new_pose[:,0],new_pose[:,1],s=0.1)
+    plt.quiver(pose_start[0],pose_start[1],np.cos(pose_start[-1]),np.sin(pose_start[-1]))
+    plt.show()
+
+if __name__ == "__main__":
+    main()
--- sensor_model.py
+++ sensor_model.py
@@ -1,3 +1,6 @@
+# This sensor model helps to calculate the likelyhood of the measurements given 
+# a position of the robot
+
 import numpy as np
 import scipy.stats
 import matplotlib.pyplot as plt
@@ -7,48 +10,51 @@ from matplotlib import cm
 def prob_density_func(x,mean,std):
     return scipy.stats.norm.pdf(x,mean,std)
 
-def calc_likelyhood_measurements_given_position(position,landmarks,measurements):
-    distance2landmarks = np.empty((np.shape(landmarks)[0],))
+def calc_likelyhood_measurements_given_position(position,landmarks,measurements,error_measurements):
+    distance2landmarks = np.empty((len(landmarks),))
     likelyhood_measurements_given_position = np.empty((np.shape(landmarks)[0],))
-    for i in range(np.shape(landmarks)[0]):
+    for i in range(len(landmarks)):
         distance2landmarks[i] = np.linalg.norm(position-landmarks[i,:])
-        likelyhood_measurements_given_position[i] = prob_density_func(measurements[i,0],measurements[i,1]+distance2landmarks[i],measurements[i,-1])
+        likelyhood_measurements_given_position[i] = prob_density_func(measurements[i],error_measurements[i,0]+distance2landmarks[i],error_measurements[i,-1])
     likelyhood = 1.0
     for i in range(np.shape(likelyhood_measurements_given_position)[0]):
         likelyhood = likelyhood*likelyhood_measurements_given_position[i]
     return likelyhood
 
-def plot_likelyhood_function(plot_vicinity,step,landmarks,measurements,plot_location):
+def plot_likelyhood_function(plot_vicinity,step,landmarks,measurements,error_measurements,plot_location):
     x = np.arange(plot_vicinity[0,0],plot_vicinity[1,0],step)
     y = np.arange(plot_vicinity[0,1],plot_vicinity[1,1],step)
     xx, yy = np.meshgrid(x,y)
     p = np.empty(np.shape(xx))
     for i in range(np.shape(x)[0]):
         for j in range(np.shape(y)[0]):
-            p[j,i] = calc_likelyhood_measurements_given_position(np.array([x[i],y[j]]),landmarks,measurements)
+            p[j,i] = calc_likelyhood_measurements_given_position(np.array([x[i],y[j]]),landmarks,measurements,error_measurements)
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.plot_surface(xx,yy,p,rstride=1,cstride=1,cmap=cm.coolwarm,alpha=0.5)
     if plot_location.any():
-        print(calc_likelyhood_measurements_given_position(plot_location[0,:],landmarks,measurements))
-        print(calc_likelyhood_measurements_given_position(plot_location[1,:],landmarks,measurements))
+        print(calc_likelyhood_measurements_given_position(plot_location[0,:],landmarks,measurements,error_measurements))
+        print(calc_likelyhood_measurements_given_position(plot_location[1,:],landmarks,measurements,error_measurements))
         for i in range(np.shape(plot_location)[0]):
-            ax.scatter(plot_location[i,0],plot_location[i,1],calc_likelyhood_measurements_given_position(plot_location[i,:],landmarks,measurements),c='b',marker='o',s=100)
+            ax.scatter(plot_location[i,0],plot_location[i,1],calc_likelyhood_measurements_given_position(plot_location[i,:],landmarks,measurements,error_measurements),c='b',marker='o',s=100)
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('likelihood')
     plt.show()
     return 
 
-landmarks = np.array([[12.0,4.0],
-                      [ 5.0,7.0]])
-measurements = np.array([[3.9,0.0,1.0],
-                         [4.5,0.0,np.sqrt(1.5)]])
-location = np.array([[10.0,8.0],
-                     [ 6.0,3.0]])
-plot_vicinity = np.array([[0,0],
-                          [17,15]])
-step = 0.5
-plot_likelyhood_function(plot_vicinity,step,landmarks,measurements,np.append(location,landmarks,axis=0))
+def main():
+    landmarks = np.array([[12.0,4.0],
+                        [ 5.0,7.0]])
+    measurements = np.array([3.9,4.5,])
+    error_measurements = np.array([[0.0,1.0],
+                                   [0.0,np.sqrt(1.5)]])
+    location = np.array([[10.0,8.0],
+                        [ 6.0,3.0]])
+    plot_vicinity = np.array([[0,0],
+                              [17,15]])
+    step = 0.5
+    plot_likelyhood_function(plot_vicinity,step,landmarks,measurements,error_measurements,np.append(location,landmarks,axis=0))
 
-
+if __name__ == "__main__":
+    main()
